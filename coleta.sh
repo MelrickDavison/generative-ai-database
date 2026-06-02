907b1297c092980c811886c09d0da920978c8d3b
12205208454 <melrick.ribeiro@nees.ufal.br>
2026-06-02 16:23:54 -0300

    feat: atualizando coleta.sh para receber arquivos locasi alem de links

--- coleta.sh
+++ coleta.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-URL="$1"
-
-if [ -z "$URL" ]; then
-    echo "Uso: ./coleta.sh <url>"
-    exit 1
-fi
-
-ARQUIVO=$(basename "$URL").html
+ENTRADA="$1"
 
 MC="/c/Users/davis/Documents/Faculdade/Orion GE-UFAL/projeto-ed2/mc.exe"
 
-echo "Baixando e enviando..."
+if [ -f "$ENTRADA" ]; then
+    ARQUIVO=$(basename "$ENTRADA")
 
-curl -L "$URL" | "$MC" pipe local/bronze/"$ARQUIVO"
+    echo "Enviando arquivo local $ARQUIVO..."
 
-if [ $? -eq 0 ]; then
-    echo "Upload concluído!"
+    "$MC" cp "$ENTRADA" local/bronze/
+
 else
-    echo "Erro no upload!"
+    ARQUIVO="$(basename "$ENTRADA").html"
+
+    if [ -z "$ENTRADA" ]; then
+        echo "Uso: ./coleta.sh <url>"
+        exit 1
+    fi
+
+    echo "Baixando e enviando $ARQUIVO..."
+
+    curl -L "$ENTRADA" | "$MC" pipe local/bronze/"$ARQUIVO"
+
+    if [ $? -eq 0 ]; then
+        echo "Upload concluído!"
+    else
+        echo "Erro no upload!"
+    fi
 fi
