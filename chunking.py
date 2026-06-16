5ff85b06512fcddd0cb303e1eea474801bad9b8f
12205208454 <melrick.ribeiro@nees.ufal.br>
2026-06-15 23:31:39 -0300

    feat: enviar chunks para postgres

--- chunking.py
+++ chunking.py
@@ -1,6 +1,6 @@
 from minio import Minio
-from io import BytesIO
 from dotenv import load_dotenv
+import psycopg2
 import os
 load_dotenv()
 
@@ -11,6 +11,22 @@ client = Minio(
     secure=False
 )
 
+conn = psycopg2.connect(
+    host=os.getenv("POSTGRES_HOST"),
+    port=os.getenv("POSTGRES_PORT"),
+    database=os.getenv("POSTGRES_DB"),
+    user=os.getenv("POSTGRES_USER"),
+    password=os.getenv("POSTGRES_PASSWORD")
+)
+
+cursor = conn.cursor()
+
+def limpar_texto(texto):
+    return (
+        texto
+        .replace('\x00', '')
+        .replace('\ufeff', '')
+    )
 
 def create_chunks(text, chunk_size=1200, overlap=200):
     chunks = []
@@ -42,6 +58,8 @@ for obj in client.list_objects("silver"):
 
     texto = response.read().decode("utf-8")
 
+    texto = limpar_texto(texto)
+    
     chunks = create_chunks(texto)
 
     nome_base = obj.object_name.replace(".md", "")
@@ -54,16 +72,22 @@ for obj in client.list_objects("silver"):
 
         dados = chunk.encode("utf-8")
 
-        client.put_object(
-            "chuncks",
-            nome_chunk,
-            BytesIO(dados),
-            length=len(dados),
-            content_type="text/plain"
+        cursor.execute(
+            """
+            INSERT INTO chunks
+            (
+                documento,
+                chunk_index,
+                tamanho,
+                conteudo
+            )
+            VALUES (%s,%s,%s,%s)
+            """,
+            (nome_base, i, len(chunk), chunk)
         )
 
-        print(f"  -> {nome_chunk}")
-
-    print()
+conn.commit()
+cursor.close()
+conn.close()
 
 print("Chunking concluído!")
