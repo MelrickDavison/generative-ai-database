fd995a12421c1e76e817d0481ec0af01ec28813f
12205208454 <melrick.ribeiro@nees.ufal.br>
2026-06-15 22:49:27 -0300

    feat: sending pdf to silver fomartted

--- transformacao.py
+++ transformacao.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 from markdownify import markdownify
 from io import BytesIO
 from dotenv import load_dotenv
+import fitz
 import os
+
 load_dotenv()
 
 client = Minio(
@@ -13,32 +15,60 @@ client = Minio(
     secure=False
 )
 
+def pdf_para_texto(pdf_bytes):
+    texto = ""
+
+    with fitz.open(stream=pdf_bytes, filetype="pdf") as pdf:
+        for pagina in pdf:
+            texto += pagina.get_text()
+
+    return texto
+
 for objeto in client.list_objects("bronze"):
 
+    nome = objeto.object_name
     resposta = client.get_object(
-        "bronze",
-        objeto.object_name
-    )
+            "bronze",
+            nome
+        )
 
-    html = resposta.read().decode("utf-8")
+    if nome.endswith(".html"):
+        html = resposta.read().decode("utf-8")
 
-    soup = BeautifulSoup(html, "html.parser")
+        soup = BeautifulSoup(html, "html.parser")
 
-    for tag in soup(["script", "style"]):
-        tag.decompose()
+        for tag in soup(["script", "style"]):
+            tag.decompose()
 
-    markdown = markdownify(str(soup))
+        markdown = markdownify(str(soup))
 
-    nome_md = objeto.object_name.replace(".html", ".md")
+        nome_saida = nome.replace(".html", ".md")
 
-    dados = markdown.encode("utf-8")
+        dados_saida = markdown.encode("utf-8")
 
-    client.put_object(
+        client.put_object(
         "silver",
-        nome_md,
-        BytesIO(dados),
-        length=len(dados),
+        nome_saida,
+        BytesIO(dados_saida),
+        length=len(dados_saida),
         content_type="text/markdown"
-    )
+        )
 
-    print(f"{objeto.object_name} -> {nome_md}")
+        print(f"{nome} -> {nome_saida}")
+    elif nome.endswith(".pdf"):
+        dados_pdf = resposta.read()
+        texto = pdf_para_texto(dados_pdf)
+        
+        nome_saida = nome.replace(".pdf", ".txt")
+        dados_saida = texto.encode("utf-8")
+        content_type = "text/plain"
+
+        client.put_object(
+            "silver",
+            nome_saida,
+            BytesIO(dados_saida),
+            length=len(dados_saida),
+            content_type="text/plain"
+        )
+
+        print(f"{nome} -> {nome_saida}")
